8b401b65f8b0353a5121cc6cc3f5900706886b95
asouza <Naruto@123>
2018-08-07 16:15:18 -0300

    - Fix the message log order in a Artemis cluster. - Improve the blacklist detection

---
+++ artemis-broker-plugin-example/README.md
@@ -0,0 +1,19 @@
+# Artemis Broker Plugin Example
+
+This example logs the message flowing IN and OUT the broker.
+
+There is a gotcha to get the order of the messages corretly in a Artemis cluster. 
+
+Use it as a reference for your own implementation.
+
+## How to deploy it
+
+1. Copy the compiled jar into `${artemis_home}/lib` directory;
+2. Add the Broker Plugin configuration into `${artemis_instance_home}/etc/broker.xml`
+```
+<broker-plugins>
+   <broker-plugin class-name="com.angelogalvao.samples.artemis.MyBrokerPlugin"/>
+</broker-plugins>
+```
+3. Start to send and to consume messages of the broker;
+4. Look the logs;
--- artemis-broker-plugin-example/src/main/java/com/angelogalvao/samples/artemis/MyBrokerPlugin.java
+++ artemis-broker-plugin-example/src/main/java/com/angelogalvao/samples/artemis/MyBrokerPlugin.java
@@ -11,10 +11,7 @@ import org.apache.activemq.artemis.core.server.plugin.ActiveMQServerPlugin;
 import org.apache.activemq.artemis.core.transaction.Transaction;
 import org.jboss.logging.Logger;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 /**
  * My custom broker plugin implementation. It only prints the message flowing in and out the broker.
@@ -26,25 +23,67 @@ public class MyBrokerPlugin implements ActiveMQServerPlugin {
     private static final Logger log = Logger.getLogger(MyBrokerPlugin.class);
 
     // In Java 9 this is significantly simpler
-    private Set<String> blackList = Collections.unmodifiableSet(new HashSet<String>(Arrays.asList(new String[]{"activemq.notifications"})));
+    private Set<String> blackList = Collections.unmodifiableSet(new HashSet<String>(Arrays.asList("activemq.", "notif")));
 
-    public void afterSend(ServerSession session, Transaction tx, Message message, boolean direct, boolean noAutoCreateQueue, RoutingStatus result) {
+    private Map<Long,String> cache = Collections.synchronizedMap(new HashMap<>());
+
+    @Override
+    public void afterMessageRoute(Message message, RoutingContext context, boolean direct, boolean rejectDuplicates, RoutingStatus result) {
 
         // skip the messages that it is in black list, like activemq.notifications
-        if( blackList.contains(message.getAddress()))
-            return;
+        if (isFromBlacklistAddress(message)) return;
 
-        log.info(String.format("MyBrokerPlugin.afterSend: Message getting OUT of the broker. Message: %s", message));
+        message.putBooleanProperty("_MyCustomBrokerPlugin_MessageWasRouted", true);
+
+    }
+
+    @Override
+    public void beforeSend(ServerSession session, Transaction tx, Message message, boolean direct, boolean noAutoCreateQueue) {
+        if (isFromBlacklistAddress(message)) return;
+
+        // it's a new message arriving to broker cluster
+        if(message.getMessageID() == 0)
+            message.putBooleanProperty("_MyCustomBrokerPlugin_isNewMessage", true);
+        else
+            message.putBooleanProperty("_MyCustomBrokerPlugin_isNewMessage", false);
 
     }
 
 
-    public void afterMessageRoute(Message message, RoutingContext context, boolean direct, boolean rejectDuplicates, RoutingStatus result) {
+
+    @Override
+    public void afterSend(ServerSession session, Transaction tx, Message message, boolean direct, boolean noAutoCreateQueue, RoutingStatus result) {
 
         // skip the messages that it is in black list, like activemq.notifications
-        if( blackList.contains(message.getAddress()))
-            return;
+        if (isFromBlacklistAddress(message)) return;
 
-        log.info(String.format("MyBrokerPlugin.afterMessageRoute: Message getting IN of the broker. Message: %s", message));
+        log.info(String.format("MyBrokerPlugin.afterSend: Message getting IN of the broker. Message: %s", message));
+
+        // delaying the route message for consistency logging.
+        if( message.getBooleanProperty("_MyCustomBrokerPlugin_MessageWasRouted") &&  message.getBooleanProperty("_MyCustomBrokerPlugin_isNewMessage")    )
+            log.info(String.format("MyBrokerPlugin.afterSend: Message getting OUT of the broker. Message: %s", message));
+
+    }
+
+    @Override
+    public void afterDeliver(ServerConsumer consumer, MessageReference reference) {
+
+        // skip the messages that it is in black list, like activemq.notifications
+        if (isFromBlacklistAddress(reference.getMessage())) return;
+
+        log.info(String.format("MyBrokerPlugin.afterDeliver: Message getting OUT of the broker. Message: %s", reference.getMessage()));
+
+    }
+
+    /**
+     * Test if message address is in blacklist.
+     * @param message the message
+     * @return true if it is in the blacklist or false if not
+     */
+    private boolean isFromBlacklistAddress(Message message) {
+        // skip the messages that it is in black list, like activemq.notifications
+        if( blackList.stream().anyMatch( address -> message.getAddress().startsWith(address) ) )
+            return true;
+        return false;
     }
 }
